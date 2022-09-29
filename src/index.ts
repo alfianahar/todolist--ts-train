5bf4e3801593e82dacb042455aa27ddface4becd
Alfian Nahar <58506749+alfianahar@users.noreply.github.com>
2022-09-29 21:25:18 +0700

    finish simple todolist

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,59 @@
-console.log('hello');
+import { uid } from 'uid';
 
-const list = document.querySelector('#list');
+type Task = {
+  id: string;
+  title: string;
+  completed: boolean;
+  createAt: Date;
+};
+
+const list = document.querySelector<HTMLUListElement>('#list');
+const form = document.getElementById('new-task-form') as HTMLFormElement | null;
+const input = document.querySelector<HTMLInputElement>('#new-task-title');
+const tasks: Task[] = loadTasks();
+tasks.forEach(addListItem);
+
+form?.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  if (input?.value == '' || input?.value == null) return;
+
+  const newTask: Task = {
+    id: uid(),
+    title: input.value,
+    completed: false,
+    createAt: new Date(),
+  };
+
+  tasks.push(newTask);
+
+  addListItem(newTask);
+  input.value = '';
+  saveTasks();
+});
+
+function addListItem(task: Task) {
+  const item = document.createElement('li');
+  const label = document.createElement('label');
+  const checkbox = document.createElement('input');
+  checkbox.addEventListener('change', () => {
+    task.completed = checkbox.checked;
+    saveTasks();
+    // console.log(task);
+  });
+  checkbox.type = 'checkbox';
+  checkbox.checked = task.completed;
+  label.append(checkbox, task.title);
+  item.append(label);
+  list?.append(item);
+}
+
+function saveTasks() {
+  localStorage.setItem('TASKS', JSON.stringify(tasks));
+}
+
+function loadTasks(): Task[] {
+  const taskJson = localStorage.getItem('TASKS');
+  if (taskJson == null) return [];
+  return JSON.parse(taskJson);
+}
